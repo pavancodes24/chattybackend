9402f7c69752dab52071de14c01363977b9730ba
chandler <pavanchandupatla24@gmail.com>
2023-12-10 16:20:21 +0530

    eslint setup done and fixed bugs

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,20 @@
-import express,{Express} from 'express';
+import express, { Express } from 'express';
 import { chattyServer } from './setupServer';
 import databaseConnection from './setupDatabase';
-import {config} from './config';
-class Application{
-    public initialize() : void {
-        this.loadConfig();
-        databaseConnection();
-        const app: Express = express();
-        const server : chattyServer = new chattyServer(app);
-        server.start();
-    }
+import { config } from './config';
+class Application {
+  public initialize(): void {
+    this.loadConfig();
+    databaseConnection();
+    const app: Express = express();
+    const server: chattyServer = new chattyServer(app);
+    server.start();
+  }
 
-    private loadConfig() : void {
-        config.validateConfig();
-    }
+  private loadConfig(): void {
+    config.validateConfig();
+  }
 }
 
-
-
-
-const application : Application = new Application();
+const application: Application = new Application();
 application.initialize();
